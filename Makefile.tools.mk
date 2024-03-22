5527575e30e64a9e11ee4e5d0a1df872c8e02ea0
Alexis Wurth <alexis.wurth@sensiolabs.com>
2024-03-23 00:24:38 +0100

    fix tools makefile

--- Makefile.tools.mk
+++ Makefile.tools.mk
@@ -7,20 +7,20 @@ Tools:
 
 ## Execute Rector in dry-run mode
 rector-dry: api/rector.php
-	$(DOCKER_COMPOSE_EXEC_PHP) bin/rector --dry-run
+	$(DOCKER_COMPOSE_EXEC_PHP) vendor/bin/rector --dry-run
 
 ## Execute Rector
 rector: api/rector.php
-	$(DOCKER_COMPOSE_EXEC_PHP) bin/rector
+	$(DOCKER_COMPOSE_EXEC_PHP) vendor/bin/rector
 
 ## Launch php-cs-fixer (dry-run mode)
 php-cs-fixer-dry: api/.php-cs-fixer.dist.php
-	$(DOCKER_COMPOSE_EXEC_PHP) bin/php-cs-fixer fix --dry-run --stop-on-violation --show-progress=dots
+	$(DOCKER_COMPOSE_EXEC_PHP) vendor/bin/php-cs-fixer fix --dry-run --stop-on-violation --show-progress=dots
 
 ## Execute PHP-CS-Fixer
 php-cs-fixer: api/.php-cs-fixer.dist.php
-	$(DOCKER_COMPOSE_EXEC_PHP) bin/php-cs-fixer fix --show-progress=dots
+	$(DOCKER_COMPOSE_EXEC_PHP) vendor/bin/php-cs-fixer fix --show-progress=dots
 
 ## Execute PHPStan
 phpstan: api/phpstan.dist.neon
-	$(DOCKER_COMPOSE_EXEC_PHP) bin/phpstan
+	$(DOCKER_COMPOSE_EXEC_PHP) vendor/bin/phpstan
