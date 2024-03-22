caa1626fdb8bf63e2c990af3700bbd8581e1eb93
Alexis Wurth <alexis.wurth@sensiolabs.com>
2024-03-23 00:33:49 +0100

    add make target for vite

--- Makefile.frontend.mk
+++ Makefile.frontend.mk
@@ -3,23 +3,15 @@ Frontend:
 
 #################################
 
-.PHONY: yarn-install yarn-watch yarn-build yarn-dev yarn-audit
+.PHONY: vite yarn-install yarn-audit
+
+## Launch Vite development server
+vite: app/package.json
+	$(DOCKER_COMPOSE_EXEC_YARN) dev --host
 
 ## Install the frontend dependencies
 yarn-install: app/package.json
 	$(DOCKER_COMPOSE_EXEC_YARN) install
-
-## Watch changes to the assets and build them
-yarn-watch: app/package.json
-	$(DOCKER_COMPOSE_EXEC_YARN) watch
-
-## Build the assets for the development env
-yarn-dev: app/package.json
-	$(DOCKER_COMPOSE_EXEC_YARN) dev
-
-## Build the assets for the production env
-yarn-build: app/package.json
-	$(DOCKER_COMPOSE_EXEC_YARN) build
 
 ## Audit the frontend dependencies
 yarn-audit: app/package.json
