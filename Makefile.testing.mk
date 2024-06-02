4ff21483b1229dee46d1f7a93bc51ca6932196e0
Alexis Wurth <alexis.wurth@sensiolabs.com>
2024-06-02 18:29:38 +0200

    fix tests

--- Makefile.testing.mk
+++ Makefile.testing.mk
@@ -6,6 +6,6 @@ Testing:
 .PHONY: phpunit
 
 ## Launch the PHPUnit tests
-phpunit: api/tests env-test create-database-schema
+phpunit: api/tests env-test create-database migrate-database
 	@$(DOCKER_COMPOSE_EXEC_PHPUNIT)
 	@$(MAKE) env-dev
