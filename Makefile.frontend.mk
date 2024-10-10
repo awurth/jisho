2be7eff0f31432fb9e0745525826da363c8a7bf9
Alexis Wurth <alexis.wurth@sensiolabs.com>
2024-10-10 14:20:30 +0200

    yarn audit fix

--- Makefile.frontend.mk
+++ Makefile.frontend.mk
@@ -3,7 +3,7 @@ Frontend:
 
 #################################
 
-.PHONY: vite yarn-install yarn-audit
+.PHONY: vite yarn-install yarn-upgrade yarn-audit
 
 ## Launch Vite development server
 vite: app/package.json
@@ -12,6 +12,10 @@ vite: app/package.json
 ## Install the frontend dependencies
 yarn-install: app/package.json
 	$(DOCKER_COMPOSE_EXEC_YARN) install
+
+## Upgrade the frontend dependencies
+yarn-upgrade: app/package.json
+	$(DOCKER_COMPOSE_EXEC_YARN) upgrade
 
 ## Audit the frontend dependencies
 yarn-audit: app/package.json
