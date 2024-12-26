dd1d2b39aba9013c9913c8036ea4dd3f8c10a08a
Alexis Wurth <alexis.wurth@sensiolabs.com>
2024-12-26 11:31:07 +0100

    api: fix authentication

--- Makefile.testing.mk
+++ Makefile.testing.mk
@@ -6,6 +6,6 @@ Testing:
 .PHONY: phpunit
 
 ## Launch the PHPUnit tests
-phpunit: api/tests env-test create-database migrate-database
+phpunit: api/tests env-test create-database migrate-database cache-clear
 	@$(DOCKER_COMPOSE_EXEC_PHPUNIT)
 	@$(MAKE) env-dev
