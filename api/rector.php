3e2e3587116cf8a01f7b94eba3ddda92cb564ec4
Alexis Wurth <alexis.wurth@sensiolabs.com>
2025-01-11 17:22:49 +0100

    api: update

--- api/rector.php
+++ api/rector.php
@@ -3,29 +3,35 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\Doctrine\Set\DoctrineSetList;
 use Rector\PHPUnit\Set\PHPUnitSetList;
 use Rector\Set\ValueObject\SetList;
 use Rector\Symfony\Set\SymfonySetList;
 
-return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->paths([
+return RectorConfig::configure()
+    ->withPaths([
         __DIR__.'/src',
         __DIR__.'/tests',
-    ]);
-
-    $rectorConfig->sets([
+    ])
+    ->withSymfonyContainerXml(__DIR__.'/var/cache/dev/App_KernelDevDebugContainer.xml')
+    ->withImportNames()
+    ->withAttributesSets(all: true)
+    ->withPreparedSets(
+        deadCode: true,
+        codeQuality: true,
+        typeDeclarations: true,
+        privatization: true,
+        instanceOf: true,
+        earlyReturn: true,
+        strictBooleans: true,
+        rectorPreset: true,
+        doctrineCodeQuality: true,
+        symfonyCodeQuality: true,
+        symfonyConfigs: true,
+    )
+    ->withSets([
         SetList::PHP_84,
-        SetList::CODE_QUALITY,
-        SetList::STRICT_BOOLEANS,
-        SetList::RECTOR_PRESET,
-        SetList::TYPE_DECLARATION,
-        SetList::EARLY_RETURN,
-        DoctrineSetList::DOCTRINE_DBAL_40,
-        DoctrineSetList::DOCTRINE_ORM_214,
-        PHPUnitSetList::PHPUNIT_100,
-        SymfonySetList::SYMFONY_71,
-        SymfonySetList::SYMFONY_CODE_QUALITY,
         SymfonySetList::SYMFONY_CONSTRUCTOR_INJECTION,
-    ]);
-};
+        SymfonySetList::SYMFONY_72,
+        PHPUnitSetList::PHPUNIT_110,
+    ])
+;
