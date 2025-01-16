88ad8073e2c677cf0352e5b423bd777d20032d2c
Alexis Wurth <alexis.wurth@sensiolabs.com>
2025-01-16 21:54:35 +0100

    app: add link for new deck button

--- app/src/components/deck-dropdown.jsx
+++ app/src/components/deck-dropdown.jsx
@@ -2,6 +2,7 @@ import { faCaretDown, faPlus } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Menu } from "@headlessui/react";
 import { useQuery } from "@tanstack/react-query";
+import {Link} from 'react-router';
 import { getDecks } from "../api/deck.js";
 import { useDeckStore } from "../stores/deck.js";
 
@@ -34,10 +35,10 @@ export default function DeckDropdown() {
           </Menu.Item>
         ))}
         <Menu.Item className="px-4 py-2 hover:bg-gray-100 rounded-md cursor-pointer whitespace-nowrap">
-          <div>
+          <Link to="/new-deck">
             <FontAwesomeIcon icon={faPlus} className="mr-1" />
             New
-          </div>
+          </Link>
         </Menu.Item>
       </Menu.Items>
     </Menu>
