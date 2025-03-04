18bab57a02bf5ad88c22a8a37292b5661d1c1dc1
Alexis Wurth <alexis.wurth@sensiolabs.com>
2025-03-04 15:39:58 +0100

    app: load quiz question on quiz start

--- app/src/components/quiz/playground.jsx
+++ app/src/components/quiz/playground.jsx
@@ -1,20 +1,24 @@
 import { useMutation } from "@tanstack/react-query";
+import {useEffect, useState} from 'react';
 import { postQuestion } from "../../api/quiz.js";
-import Button from "../button.jsx";
 
 export default function Playground({ quiz }) {
-  const mutation = useMutation({
+  const [question, setQuestion] = useState(null);
+  const postQuestionMutation = useMutation({
     mutationFn: () => {
       return postQuestion(quiz.id);
     },
     onSuccess: (data) => {
-      console.log(data);
+      setQuestion(data);
     },
   });
 
+  useEffect(() => {
+    postQuestionMutation.mutate();
+  }, []);
+
   return (
     <>
-      <Button onClick={() => mutation.mutate()}>New question</Button>
     </>
   );
 }
