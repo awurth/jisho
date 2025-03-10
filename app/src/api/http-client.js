e5bf6c06748e0b613abeaefefdbd4203289e36fe
Alexis Wurth <alexis.wurth@sensiolabs.com>
2025-03-10 14:35:08 +0100

    app: fix interceptors

--- app/src/api/http-client.js
+++ app/src/api/http-client.js
@@ -1,4 +1,6 @@
 import axios from "axios";
+import {router} from '../routes.jsx';
+import {useUserStore} from '../stores/user.js';
 
 const httpClient = axios.create({
   baseURL: "https://api.jisho.localhost",
@@ -6,5 +8,34 @@ const httpClient = axios.create({
 
 httpClient.defaults.headers.patch["Content-Type"] =
   "application/merge-patch+json";
+
+httpClient.interceptors.request.use(
+  function (config) {
+    const token = localStorage.getItem("token");
+
+    if (token && !config.headers.Authorization) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  },
+);
+
+httpClient.interceptors.response.use(
+  function (response) {
+    return response;
+  },
+  function (error) {
+    if (error.response?.status === 401) {
+      useUserStore.setState({ user: null });
+      router.navigate("/login");
+    }
+
+    return Promise.reject(error);
+  },
+);
 
 export default httpClient;
--- app/src/main.jsx
+++ app/src/main.jsx
@@ -3,7 +3,6 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router";
-import "./interceptors.jsx";
 import { router } from "./routes.jsx";
 import "./index.scss";
 
