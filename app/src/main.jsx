b04f83ab63c3cb432be520e88b788566796e26e9
Alexis Wurth <alexis.wurth@sensiolabs.com>
2025-03-17 15:57:17 +0100

    app: update eslint config

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -1,7 +1,7 @@
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import React from "react";
-import ReactDOM from "react-dom/client";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router";
 import { router } from "./routes.jsx";
 import "./index.css";
@@ -14,13 +14,13 @@ const queryClient = new QueryClient({
   },
 });
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
     <GoogleOAuthProvider clientId="130033880819-ne189jjdghlha0e0do0fhc5ig0lb230i.apps.googleusercontent.com">
       <QueryClientProvider client={queryClient}>
         <RouterProvider router={router} />
         {/*<ReactQueryDevtools initialIsOpen={false} />*/}
       </QueryClientProvider>
     </GoogleOAuthProvider>
-  </React.StrictMode>,
+  </StrictMode>,
 );
